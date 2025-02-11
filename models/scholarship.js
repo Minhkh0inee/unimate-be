00674cfd68030bc13e9c4fe056708020f247f95b
Minhkh0inee <mkhoi1776@gmail.com>
2025-02-11 15:06:24 +0700

    Update Models Scholarship

--- models/scholarship.js
+++ models/scholarship.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const scholarshipSchema = new mongoose.Schema({
   university: { type: String, required: true },
   name: { type: String, required: true },
-  type: { type: String, enum: ["Full-ride", "Full-tuition", "Partial"] },
-  degree_level: { type: String, enum: ["Undergraduate", "Graduate"] },
+  type: { type: String, enum: ["Full-ride", "Full-tuition", "Partial"],require: false} ,
+  degree_level: { type: String, enum: ["Undergraduate", "Graduate"], require: false},
   country: { type: String, required: true },
   deadline: { type: Date, required: false },
+  min_gpa: {type: String, require: false},
+  major: {type: String, require: false},
+  description: {type: String, require: false},
+  website_link: {type: String, require: false},
   notion_url: { type: String }, 
 }, { timestamps: true });
 
