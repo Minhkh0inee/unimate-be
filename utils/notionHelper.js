30766a9d1174b0aa94593bd15adf491c7a0b53f1
Minhkh0inee <mkhoi1776@gmail.com>
2025-02-23 08:56:31 +0700

    Fix NotionHelper Schema

--- utils/notionHelper.js
+++ utils/notionHelper.js
@@ -1,12 +1,13 @@
 const transformNotionToScholarship = (notionData) => {
     return {
-      university: notionData.properties.University?.title?.[0]?.plain_text || "Unknown",
-      name: notionData.properties.Title?.rich_text?.[0]?.plain_text || "Unknown",
+      university: notionData.properties.University?.rich_text[0]?.plain_text || "Unknown",
+      name: notionData.properties['Scholarship Name'].title[0]?.plain_text || "Unknown",
       type: notionData.properties.Type?.select?.name || "Partial",
       degree_level: notionData.properties["Degree Level"]?.select?.name || "Undergraduate",
       country: notionData.properties.Country?.select?.name || "Unknown",
       deadline: notionData.properties.Deadline?.date?.start || null,
       notion_url: notionData.url || null,
+      website_link: notionData.properties['Website Link'].url
     };
   };
   
